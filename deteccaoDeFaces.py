e33e4a975e66508f4c39d7955dbc1075f0d38435
unknown <glimadefreitasgit2002@gmail.com>
2024-09-10 21:44:17 -0300

    Parâmetros haarcascade 2

--- deteccaoDeFaces.py
+++ deteccaoDeFaces.py
@@ -1,7 +1,7 @@
 import cv2 
 
 
-imagem = cv2.imread('C:\\Users\\Guilherme L Freitas\\OneDrive\\Desktop\\projetos\\deteccaoDeFaces\\img\\people1.jpg')
+imagem = cv2.imread('C:\\Users\\Guilherme L Freitas\\OneDrive\\Desktop\\projetos\\deteccaoDeFaces\\img\\people2.jpg')
 
 imagem = cv2.resize(imagem, (800, 600))
 
@@ -11,7 +11,8 @@ imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 detector_facial = cv2.CascadeClassifier('C:\\Users\\Guilherme L Freitas\\OneDrive\\Desktop\\projetos\\deteccaoDeFaces\\cascade\\haarcascade_frontalface_default.xml')
 
 
-deteccoes = detector_facial.detectMultiScale(imagem_cinza, scaleFactor=1.09)
+deteccoes = detector_facial.detectMultiScale(imagem_cinza, scaleFactor=1.2, minNeighbors=3,
+                                             minSize=(32,32), maxSize=(100,100))
 
 for x, y, w, h in deteccoes:
     # print(x, y, w, h)
