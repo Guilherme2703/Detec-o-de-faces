d13d25f39b5f0f9048e130a94c9816ce3d94609f
unknown <glimadefreitasgit2002@gmail.com>
2024-09-21 13:50:25 -0300

    Detecção de Relogio

--- deteccaoDeFaces.py
+++ deteccaoDeFaces.py
@@ -1,6 +1,6 @@
 import cv2 
 
-imagem = cv2.imread('C:\\Users\\Guilherme L Freitas\\OneDrive\\Desktop\\projetos\\deteccaoDeFaces\\img\\Leonardo_Phoenix_A_person_with_a_focused_expression_wearing_a_0.jpg')
+imagem = cv2.imread('C:\\Users\\Guilherme L Freitas\\OneDrive\\Desktop\\projetos\\deteccaoDeFaces\\img\\clock.jpg')
 
 imagem = cv2.resize(imagem, (600, 700))
 
@@ -9,23 +9,29 @@ imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
 detector_facial = cv2.CascadeClassifier('C:\\Users\\Guilherme L Freitas\\OneDrive\\Desktop\\projetos\\deteccaoDeFaces\\cascade\\haarcascade_frontalface_default.xml')
 detector_olhos = cv2.CascadeClassifier('C:\\Users\\Guilherme L Freitas\\OneDrive\\Desktop\\projetos\\deteccaoDeFaces\\cascade\\haarcascade_frontalface_default.xml')
-
-
+detector_relogios = cv2.CascadeClassifier('C:\\Users\\Guilherme L Freitas\\OneDrive\\Desktop\\projetos\\deteccaoDeFaces\\cascade\\clocks.xml')
+detector_carros = cv2.CascadeClassifier('C:\\Users\\Guilherme L Freitas\\OneDrive\\Desktop\\projetos\\deteccaoDeFaces\\cascade\\')
+# detector de faces
 # deteccoes = detector_facial.detectMultiScale(imagem_cinza, scaleFactor=1.01, minNeighbors=4,
 #                                              minSize=(32,32), maxSize=(100,100))
 
+# deteccoes = detector_facial.detectMultiScale(imagem_cinza, scaleFactor=1.01)
+# for x, y, w, h in deteccoes:
+#     # print(x, y, w, h)
+#     cv2.rectangle(imagem, (x, y), (x + w, y + h), (0, 255, 0), 5)
 
-deteccoes = detector_facial.detectMultiScale(imagem_cinza, scaleFactor=1.01)
+# # detecto de olhos
+# deteccoes_olhos = detector_olhos.detectMultiScale(imagem_cinza, scaleFactor=1.01, minNeighbors=1, maxSize=(90, 90), minSize=(80, 80))
+# for (x, y, w, h) in deteccoes_olhos:
+#     # print(w,h)
+#     cv2.rectangle(imagem, (x, y), (x + w, y + h), (0,0,255), 2)
 
-for x, y, w, h in deteccoes:
-    # print(x, y, w, h)
-    cv2.rectangle(imagem, (x, y), (x + w, y + h), (0, 255, 0), 5)
+# detecto de relogios 
+# deteccoes_relogio = detector_relogios.detectMultiScale(imagem_cinza, scaleFactor=1.5, minNeighbors=1, minSize=(150,150))
+# for (x, y, w, h) in deteccoes_relogio:
+#     print(w, h)
+#     cv2.rectangle(imagem, (x, y), (x + w, y + h), (0, 0, 200), 2)
 
-
-deteccoes_olhos = detector_olhos.detectMultiScale(imagem_cinza, scaleFactor=1.01, minNeighbors=1, maxSize=(90, 90), minSize=(80, 80))
-for (x, y, w, h) in deteccoes_olhos:
-    # print(w,h)
-    cv2.rectangle(imagem, (x, y), (x + w, y + h), (0,0,255), 2)
 
 cv2.imshow('mat', imagem)
 cv2.waitKey(0)
